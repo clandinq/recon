882c4d71a3ece2b3a03d0550633155bdfe9ea46e
clandinq <cesar.landin@itam.mx>
2023-04-25 16:11:57 -0500

    Minimal files

---
+++ render.R
@@ -0,0 +1,8 @@
+pacman::p_load(here)
+here()
+rmarkdown::render_site(here("site"),
+                       output_format = "bookdown::gitbook",
+                       encoding = "UTF-8")
+old_path <- here("site", "docs")
+new_dir <- here("docs")
+file.rename(old_path, new_dir)
