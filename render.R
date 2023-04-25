b9873b90b17565c025e9904ef553a5cd41f62b13
clandinq <cesar.landin@itam.mx>
2023-04-25 17:45:50 -0500

    Google site verification

--- render.R
+++ render.R
@@ -5,4 +5,5 @@ rmarkdown::render_site(here("site"),
                        encoding = "UTF-8")
 old_path <- here("site", "docs")
 new_dir <- here("docs")
+suppressWarnings(unlink(new_dir, recursive = TRUE))
 file.rename(old_path, new_dir)
